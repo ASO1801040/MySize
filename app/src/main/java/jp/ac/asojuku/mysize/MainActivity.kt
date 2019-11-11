87e8fa7193c1c2d6e8c40fbb785e3f5fb86395e7
ASO1801040 <57512936+ASO1801040@users.noreply.github.com>
2019-11-11 12:26:09 +0900

    pushの練習

--- app/src/main/java/jp/ac/asojuku/mysize/MainActivity.kt
+++ app/src/main/java/jp/ac/asojuku/mysize/MainActivity.kt
@@ -8,7 +8,7 @@ import androidx.core.content.edit
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
+//    pushの練習コメント
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
